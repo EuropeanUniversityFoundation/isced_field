41d50422886f17eaab327e07f0b4db063ae24624
Tiago Simões <tiago.simoes@uni-foundation.eu>
2025-06-23 15:19:30 +0200

    Cleans up validation constraint

--- src/Plugin/Validation/Constraint/ValidIscedFConstraint.php
+++ src/Plugin/Validation/Constraint/ValidIscedFConstraint.php
@@ -4,18 +4,24 @@ declare(strict_types=1);
 
 namespace Drupal\isced_field\Plugin\Validation\Constraint;
 
-use Symfony\Component\Validator\Constraint;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Validation\Attribute\Constraint;
+use Symfony\Component\Validator\Constraint as SymfonyConstraint;
 
 /**
  * Provides a Valid ISCED-F constraint.
- *
- * @Constraint(
- *   id = "ValidIscedF",
- *   label = @Translation("Valid ISCED-F", context = "Validation"),
- * )
  */
-final class ValidIscedFConstraint extends Constraint {
+#[Constraint(
+  id: 'ValidIscedF',
+  label: new TranslatableMarkup('Valid ISCED-F', [], ['context' => 'Validation'])
+)]
+final class ValidIscedFConstraint extends SymfonyConstraint {
 
+  /**
+   * The error message if the value is not a valid ISCED-F field of study.
+   *
+   * @var string
+   */
   public string $message = '%value is not a valid ISCED-F field of study.';
 
 }
